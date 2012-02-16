2531c8c7f5ce679cb5722eda7b0dcd378727baf6
rhodes <digital.imgaineer@gmail.com>
2012-02-15 20:47:45 -0800

    initial commit - rails s working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RhodesRor::Application.config.secret_token = 'f7d0ea26082ab1db8b24770b48e1a0e432ad87d5e0f31013620d412067a9f62024920f335e7fa1138bc6546a46c1e615b18a29eaa6436773176a636d0c440afa'
