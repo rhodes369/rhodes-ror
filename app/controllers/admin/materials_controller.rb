3b32b02154ac0d7d3c23244396af3a3b109fde06
doktor J <doktorj@gmail.com>
2012-03-01 13:24:31 -0800

    now have material type saving for edit material...and as for the finish_ids multi-select box, this has been stumping me on what the best way to do this with haml is...so i commented the finishes section out of the edit material form until me or Eph figures out the right haml syntax

--- app/controllers/admin/materials_controller.rb
+++ app/controllers/admin/materials_controller.rb
@@ -1,10 +1,11 @@
 class Admin::MaterialsController < ApplicationController
   
   layout 'admin/layouts/application'
-  
+
   def index
+    @materials = Material.all 
     @material = Material.new
-    @materials = Material.all #.order 'DESC'    
+    render :index
   end  
     
   def create
@@ -23,20 +24,31 @@ class Admin::MaterialsController < ApplicationController
   
   def edit
     @material = Material.find(params[:id])
+    @all_finishes = Finish.order('title ASC')
   end  
   
   def update
     @material = Material.find(params[:id])
-
+    return if @material.nil?
+      
+    #render text: params[:material_finishes][:finish_ids] 
+    
+    @material.reset_finishes()
+    @material.populate_finishes params[:material][:finish_ids]
+    
+    #params[:material_finishes].delete
+    
     respond_to do |format|
       if @material.update_attributes(params[:material])
-        format.html { redirect_to admin_materials_path, notice: 'Material was successfully updated.' }
-        format.json { head :no_content }
+        
+        format.html { redirect_to admin_materials_path, :notice => 'Material was successfully updated.' }
+        #format.json { head :no_content }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @material.errors, status: :unprocessable_entity }
+       format.html { render action: "edit" }
+       #format.json { render json: @material.errors, status: :unprocessable_entity }
       end
     end
+
   end  
   
   
