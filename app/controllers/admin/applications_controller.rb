f2b9201dc0aae387dad2e8f54cc854018d77def6
doktor J <doktorj@gmail.com>
2012-03-12 03:31:06 -0700

    added applications controller and got add/edit applications working from /admin

---
+++ app/controllers/admin/applications_controller.rb
@@ -0,0 +1,49 @@
+class Admin::ApplicationsController < ApplicationController
+  
+  def index
+    @applications = Application.all
+    @application = Application.new
+  end
+  
+  def new
+    @applications = Application.all
+    @application = Application.new
+  end
+
+  def edit
+    @applications = Application.all
+    @application = Application.find(params[:id])
+  end
+  
+  def create
+    @application = Application.new(params[:application] )
+
+    respond_to do |format|
+      if @application.save
+        format.html { redirect_to admin_applications_path, :notice => 'Application was Saved' }
+        format.json { render json: @application, status: :created, location: @finish }
+      else
+        flash[:error] = 'Problem Saving Application'
+        format.html { redirect_to admin_applications_path }
+        format.json { render json: @application.errors, status: :unprocessable_entity }
+      end
+    end
+  end  
+  
+  def update
+    @application = Application.find(params[:id])
+    return if @application.nil?
+    
+    respond_to do |format|
+      if @application.update_attributes(params[:application])   
+                  
+        format.html { redirect_to admin_applications_path, notice: 'Application Updated' }
+        format.json { head :no_content, status: :success }
+      else
+        format.html { render action: "edit", error: 'Problem updating Application' }
+        format.json { render json: @application.errors, status: :unprocessable_entity }
+      end
+    end
+  end  
+  
+end
