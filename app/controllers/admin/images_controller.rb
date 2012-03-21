076e5428c9042d3fb42bd77be410e94c0354ad91
doktor J <doktorj@gmail.com>
2012-03-21 06:14:20 -0700

    made it so the first image uploaded gets autoset as default image

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -33,12 +33,15 @@ class Admin::ImagesController < ApplicationController
   def create
     @image = Image.new(params[:image])
     @material = Material.find(params[:image][:material_id])
- 
-    respond_to do |format|
-      if @image.save
-        format.html { redirect_to edit_admin_material_path(@material), 
-                      notice: 'Image was successfully added.' }
-        format.json { render json: @image, status: :created, location: @image }
+    return if @material.nil?
+        
+      respond_to do |format|
+        if @image.save        
+          # set as default image if it's the only image 
+          @material.set_default_image(@image.id) if @material.images.count == 1
+          
+          format.html { redirect_to edit_admin_material_path(@material), notice: 'Image was successfully added.' }
+          format.json { render json: @image, status: :created, location: @image }
       else
         format.html { redirect_to edit_admin_material_path(@material), alert: 'Problem uploading Image' }
         format.json { render json: @image.errors, status: :unprocessable_entity }
@@ -52,19 +55,19 @@ class Admin::ImagesController < ApplicationController
     @image = Image.find(params[:id])
     @material = Material.find(@image.material_id)
     
+    # TODO - move this part into model
     # set a new alternative default_image_id if the current one is getting axed
     if @material.default_image_id == @image.id
       if @material.images.count > 1
         @alt_images_ids = @material.images.map(&:id)
-        unless @alt_image_ids == @image_id
+        if @alt_image_ids != @image_id
           @material.default_image_id = @alt_image_ids.first
         else
           @material.default_image_id = @alt_image_ids.last
         end
+      end
     end    
           
-        
-    
     # detatch/delete all related paperclip images
     @image.image = nil
     @image.save 
