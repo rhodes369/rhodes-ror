85759d947bbf3993ea4d2cca43e4fd153b47208a
doktor J <doktorj@gmail.com>
2012-03-29 05:38:42 -0700

    refactored mat_types integration tests to use less code and be more reusable

--- test/integration/material_types_test.rb
+++ test/integration/material_types_test.rb
@@ -2,31 +2,28 @@ require 'test_helper'
 
 class MaterialTypesTest < ActionDispatch::IntegrationTest
   
-  test "left sidebar should link to edit page" do 
-    mat_type = Factory(:material_type, title: 'firebrick')
+  def setup
+    @mat_type_1 = Factory(:material_type, id: 1, title: 'firebrick')
+    @mat_type_2 = Factory(:material_type, id: 2, title: 'granite')    
+
+    2.times { Factory(:material, material_type_id: 1) }
+    @mat_3 = Factory(:material, material_type_id: 2)       
+  end
   
+  test "left sidebar links should link to edit page" do 
     visit admin_material_types_url
   
     within 'div#content-left' do
       click_link 'firebrick'
-      assert_equal edit_admin_material_type_path(mat_type), current_path
-      edit_admin_material_type_path(mat_type)
+      assert_equal edit_admin_material_type_path(@mat_type_1), current_path
     end 
   end 
  
   test "clicking delete button should delete mat type and all mat relations" do  
-  
-    mat_type_1 = Factory(:material_type, id: 1, title: 'firebrick')
-    mat_type_2 = Factory(:material_type, id: 2, title: 'granite')
-    
-    mat_1 = Factory(:material, material_type_id: 1)
-    mat_2 = Factory(:material, material_type_id: 1)
-    mat_3 = Factory(:material, material_type_id: 2)
-    
-    visit edit_admin_material_type_path(mat_type_1)
+    visit edit_admin_material_type_path(@mat_type_1)
     
     within 'div#content-center' do
-      click_button 'delete'
+      click_button 'delete'  
       assert_equal MaterialType.where(title: 'firebrick').exists?, false
       assert_equal Material.where(material_type_id: 1).count, 0
     end    
