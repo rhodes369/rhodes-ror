2fb5d5b92d7864b606e86185960cedc5cf0606f7
doktor J <doktorj@gmail.com>
2012-03-29 06:31:10 -0700

    cleaned up gemfile + added 2 more mat_type integration tests, to make sure create new mat type works, and that you can only create unique mat type titles

--- test/integration/material_types_test.rb
+++ test/integration/material_types_test.rb
@@ -7,7 +7,7 @@ class MaterialTypesTest < ActionDispatch::IntegrationTest
     @mat_type_2 = Factory(:material_type, id: 2, title: 'granite')    
 
     2.times { Factory(:material, material_type_id: 1) }
-    @mat_3 = Factory(:material, material_type_id: 2)       
+    @mat_3 = Factory(:material, material_type_id: 2)      
   end
   
   test "left sidebar links should link to edit page" do 
@@ -18,15 +18,27 @@ class MaterialTypesTest < ActionDispatch::IntegrationTest
       assert_equal edit_admin_material_type_path(@mat_type_1), current_path
     end 
   end 
- 
+
+  test "clicking create button twice with same title should only save once" do
+    2.times do 
+      visit admin_material_types_path
+    
+      within 'div#content-center' do 
+        fill_in 'material_type_title', with: 'bamboo'
+        click_button 'create_material_type'
+        assert_equal MaterialType.where(title: 'bamboo').count, 1
+      end
+    end
+  end
+
   test "clickng update button should save title" do
     visit edit_admin_material_type_path(@mat_type_1)
     
     within 'div#content-center' do
       new_title = 'test369'
-      fill_in 'material_type_title', with: new_title
-      click_button 'update'
-      assert_equal MaterialType.where(id: @mat_type_1).first.title, new_title
+      fill_in 'material_type_title', with: 'marble'
+      click_button 'update_material_type'
+      assert_equal MaterialType.where(id: @mat_type_1).first.title, 'marble'
     end
   end
  
