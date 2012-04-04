d3ec841e59b532c17371d8d9f17495b34654a689
doktor J <doktorj@gmail.com>
2012-04-04 03:49:37 -0700

    cleaned up and tweaked dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,12 +44,8 @@ RhodesRor::Application.configure do
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
-    ## Bullet.growl = true # busted
-    # Bullet.xmpp = { :account => 'bullets_account@jabber.org',
-    #                 :password => 'bullets_password_for_jabber',
-    #                 :receiver => 'your_account@jabber.org',
-    #                 :show_online_status => true }
-    Bullet.rails_logger = true
+    Bullet.growl = false # seems busted
+    Bullet.rails_logger = false
     Bullet.disable_browser_cache = true
   end
 end
