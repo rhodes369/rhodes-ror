b282d17a91784b17c3f7277716b70adc5e120b28
doktor J <doktorj@gmail.com>
2012-04-06 03:59:18 -0700

    resetup/refactored test_helper to use guard/spork again, all tests passing, plus went thru the Gemfile gem by gem and added a ~> and specific version # to lock us down until we know we're ready to upgrade them

--- Gemfile
+++ Gemfile
@@ -2,31 +2,20 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.2'
 gem 'sqlite3', '1.3.5'
-gem 'haml-rails'
 gem 'slugged', '~> 1.0.1'
 gem "paperclip", "~> 3.0"
-gem 'bcrypt-ruby', '3.0.1'
+gem 'coffee-filter'
+gem 'haml-rails'
 
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
-  gem 'coffee-filter'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer'
-
   gem 'uglifier', '>= 1.0.3'
 end
 
 gem 'jquery-rails', '>= 2.0.1'
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
 
 # Use unicorn as the web server
 # gem 'unicorn'
@@ -37,29 +26,22 @@ gem 'jquery-rails', '>= 2.0.1'
 # To use debugger
 gem 'ruby-debug19' #, :require => 'ruby-debug' # the require breaks on my local -drJ
 
-
 group :development, :test do
-  gem 'growl', '1.0.3'
-  #gem 'test-unit', '2.4.8'
-  gem 'ruby-prof'
-  gem 'guard-spork'  
-  gem 'spork', '~> 1.0rc'
-  gem 'spork-testunit'
-  gem 'rb-fsevent', '0.4.3.1', :require => false
+  gem 'growl', '~> 1.0.3'
   gem 'guard-test', '0.4.3'
-  gem 'faker', '1.0.1'
-  #gem 'factory_girl_rails', '~> 1.4.0' # old school
-  # using factory_girl master for now since it has a depreciation warning patch
-  gem 'factory_girl', :git => 'git://github.com/thoughtbot/factory_girl.git', :branch => "master"
-  gem 'factory_girl_rails', :git => 'git://github.com/thoughtbot/factory_girl_rails.git', :branch => "master", :require => false
-  
+  gem 'guard-spork', '~> 0.5.2'
+  gem 'spork-testunit'
+  gem 'faker', '~> 1.0.1'
+  gem 'factory_girl_rails', '~> 3.0'
 end
 
 group :test do
-  gem 'capybara', '1.1.2'
-  gem 'shoulda', '3.0.1'
-  gem 'mocha', '0.10.5'
-  gem 'turn', '0.9.4'
-  gem 'minitest'
-  gem 'database_cleaner'
+  gem 'capybara', '~> 1.1.2'
+  gem 'shoulda', '~> 3.0.1'
+  gem 'mocha', '~> 0.10.5'
+  gem 'turn', '~> 0.9.4'
+  gem 'database_cleaner', '~> 0.7.2'
+  gem 'minitest', '~> 2.11.4', :require => false # for turn
+  gem 'rb-fsevent', '~> 0.4.3.1', :require => false
+  gem 'ruby-prof', :require => false
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,41 +1,68 @@
+require 'rubygems'
+require 'spork'
 
-# This code will be run each time you run your specs.
-ENV["RAILS_ENV"] = "test"
-
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-require 'capybara/rails'
-require 'faker'
-require 'factory_girl_rails'
-require 'database_cleaner'
-
-
-DatabaseCleaner.strategy = :truncation
-#DatabaseCleaner.clean
-#FactoryGirl.find_definitions
-
-class ActiveSupport::TestCase
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
   
-  self.use_transactional_fixtures = true
+  # This code will be run each time you run your specs.
+  ENV["RAILS_ENV"] = "test"
   
-  def teardown
-    DatabaseCleaner.clean    
+  require File.expand_path('../../config/environment', __FILE__)
+  require 'rails/test_help'
+  require 'capybara/rails'
+  require 'factory_girl_rails'
+  require 'faker'
+  require 'database_cleaner'
+  
+  DatabaseCleaner.strategy = :truncation
+  
+  class ActiveSupport::TestCase
+    # Add more helper methods to be used by all tests here...
   end
-  
-  # Add more helper methods to be used by all tests here...
+
+  class ActionDispatch::IntegrationTest
+    include Capybara::DSL
+    
+    def teardown
+      DatabaseCleaner.clean
+      Capybara.reset_sessions! 
+      Capybara.use_default_driver 
+    end 
+  end  
 end
 
-class ActionDispatch::IntegrationTest
-  include Capybara::DSL
-  
-  self.use_transactional_fixtures = true
-  
-  #FactoryGirl.reload
-  
-  def teardown
-    DatabaseCleaner.clean
-    Capybara.reset_sessions! 
-    Capybara.use_default_driver 
-  end 
-end  
+Spork.each_run do
+  FactoryGirl.reload
+end
+
+# --- Instructions ---
+# Sort the contents of this file into a Spork.prefork and a Spork.each_run
+# block.
+#
+# The Spork.prefork block is run only once when the spork server is started.
+# You typically want to place most of your (slow) initializer code in here, in
+# particular, require'ing any 3rd-party gems that you don't normally modify
+# during development.
+#
+# The Spork.each_run block is run each time you run your specs.  In case you
+# need to load files that tend to change during development, require them here.
+# With Rails, your application modules are loaded automatically, so sometimes
+# this block can remain empty.
+#
+# Note: You can modify files loaded *from* the Spork.each_run block without
+# restarting the spork server.  However, this file itself will not be reloaded,
+# so if you change any of the code inside the each_run block, you still need to
+# restart the server.  In general, if you have non-trivial code in this file,
+# it's advisable to move it into a separate file so you can easily edit it
+# without restarting spork.  (For example, with RSpec, you could move
+# non-trivial code into a file spec/support/my_helper.rb, making sure that the
+# spec/support/* files are require'd from inside the each_run block.)
+#
+# Any code that is left outside the two blocks will be run during preforking
+# *and* during each_run -- that's probably not what you want.
+#
+# These instructions should self-destruct in 10 seconds.  If they don't, feel
+# free to delete them.
 
