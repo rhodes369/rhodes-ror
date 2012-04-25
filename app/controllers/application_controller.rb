c8e8a1569fbe64e8e307ad009fa33aa8fe49f5da
doktor J <doktorj@gmail.com>
2012-04-25 00:26:48 -0700

    prod doesn't seem to like the / redirect, so trying /index.html instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def index
-    redirect_to '/' # default to /public/index.html
+    redirect_to '/index.html' # default to /public/index.html
   end
   
   # redirect to /public/index.html with alert, and log it
   def bad_route
-    redirect_to '/', alert: "Sorry, the url: #{request.fullpath} was not found"
+    redirect_to '/index.html', alert: "Sorry, the url: #{request.fullpath} was not found"
     logger.error "Bad route requested for #{request.fullpath}"
   end
 end
