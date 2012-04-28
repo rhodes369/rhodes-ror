116c5201031dbca01517240ed078bfc11728a755
doktor J <doktorj@gmail.com>
2012-04-27 19:49:08 -0700

    renamed sessions controller to auth_sessions_controller and all references to, and removed users controller since we are only adding users manually for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 RhodesRor::Application.routes.draw do
   
-  get "logout" => "sessions#destroy", :as => "logout"
-  get "login" => "sessions#new", :as => "login"
+  get "logout" => "auth_sessions#destroy", :as => "logout"
+  get "login" => "auth_sessions#new", :as => "login"
   
-  resources :users, :only => [:new, :create]
-  resources :sessions, :only => [:new, :create, :destroy]
+  # resources :users, :only => [:new, :create]
+  resources :auth_sessions, :only => [:new, :create, :destroy]
   
   resources :materials, :only => [:index, :show] do
     put :search, :to => 'materials#search'
