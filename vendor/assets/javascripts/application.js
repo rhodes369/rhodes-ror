a68dfe4a67044ca52aa2e5c99555b997a2aee690
Ephraim Peniston <useruser@Ephraim-Peniston-2.local>
2012-06-07 23:21:02 -1000

    Added multiselect js and css files. Followed demo directions and adjusted haml select to trigger, but functions seem to be missing. Created optgroups in haml with placeholder content for minimum thickness.

--- vendor/assets/javascripts/application.js
+++ vendor/assets/javascripts/application.js
@@ -1,7 +1,9 @@
 //= require jquery-tools_no-jq.1.2.7
+//= require jqueryUI-widgetFactory
 //= require thumb_scroll
 //= require dropdown
 //= require jquery.cookie
 //= require images
 //= require toggle_menu
+//= require multiselect
 
---
+++ vendor/assets/javascripts/multiselect.js.coffee
@@ -0,0 +1,1 @@
+$(".finishesImg").multiselect()
