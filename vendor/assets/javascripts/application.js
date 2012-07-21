4b32d0534513273ab7b04028a84cefaeb81a4879
PlaceboArts <ephraim@placeboarts.com>
2012-07-20 14:09:23 -1000

    Added jquery ui-1.8 to allow multiselect. pulled multiselect initializer into body (for now) and removed its jquery partial. Updated styles for multiselect and converted css to sass Having trouble including stylesheet partials from vendors/stylesheets, so for now, multiselect is in assets/admin

--- vendor/assets/javascripts/application.js
+++ vendor/assets/javascripts/application.js
@@ -1,5 +1,6 @@
+
 //= require jquery-tools_no-jq.1.2.7
-//= require jqueryUI-widgetFactory
+//= require jquery-ui-1.8.21.min
 //= require thumb_scroll
 //= require dropdown
 //= require jquery.cookie
