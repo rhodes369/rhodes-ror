7b3ccbe00523ac29154ee2af508e69859c369e73
PlaceboArts <ephraim@placeboarts.com>
2012-07-29 23:18:29 -0700

    Adjusted styling on icon and 1st image radios to be on the same line and added min thickness select dropdown Removed jquery multiselect and ui widget factory

--- vendor/assets/javascripts/application.js
+++ vendor/assets/javascripts/application.js
@@ -1,9 +1,7 @@
 //= require jquery-tools_no-jq.1.2.7
-//= require jqueryUI-widgetFactory
 //= require thumb_scroll
 //= require dropdown
 //= require jquery.cookie
 //= require images
 //= require toggle_menu
-//= require multiselect
 
--- vendor/assets/javascripts/multiselect.js.coffee
+++
@@ -1,1 +0,0 @@
-$(".finishesImg").multiselect()
